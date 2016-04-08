804ead861894ad0a44d2d4c9cc367342d37cc6a3
Kevin Ward <kevinward@fescorp.com>
2016-04-08 13:46:02 -0500

    Add Client and ProductArea models.

--- features/models.py
+++ features/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    """Client associated with feature request"""
+
+    # Name of client
+    name = models.CharField('Name', max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class ProductArea(models.Model):
+    """Product Area category for feature requests"""
+
+    # Name of product area
+    name = models.CharField('Name', max_length=200, unique=True)
+
+    def __str__(self):
+        return self.name
--- features/tests.py
+++ features/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Client, ProductArea
+
+class ModelSanityTests(TestCase):
+    def test_client(self):
+        name = 'ClientA'
+        # Check that the universe is roughly logical
+        client_A = Client(name=name)
+        client_A.save()
+        assert client_A.name == name
+
+        # This should exist, right?
+        loaded_client = Client.objects.get(name=name)
+
+        # Cool, it does. But is it what we think it is?
+        assert loaded_client
+        assert loaded_client.id == client_A.id
+        assert loaded_client.name == client_A.name
+        # I guess it is.
+
+    def test_product_area(self):
+        name = 'Sales'
+
+        area = ProductArea(name=name)
+        area.save()
+        assert area.name == name
+
+        loaded_area = ProductArea.objects.get(name=name)
+
+        assert loaded_area
+        assert loaded_area.id == area.id
+        assert loaded_area.name == area.name
