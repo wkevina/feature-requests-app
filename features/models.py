7d6f99aaed849f1b630091ed972066b138f34af8
Kevin Ward <kevinward@fescorp.com>
2016-04-08 16:45:12 -0500

    Add FeatureRequest model and tests.

--- features/models.py
+++ features/models.py
@@ -17,3 +17,43 @@ class ProductArea(models.Model):
 
     def __str__(self):
         return self.name
+
+class FeatureRequest(models.Model):
+    """Information about a new feature a client has requested"""
+
+    # Short, descriptive name of the feature request.
+    # Max length is 200 characters; better than a tweet!
+    title = models.CharField('Title', max_length=200)
+
+    # Longer description of the feature request
+    description = models.TextField('Description')
+
+    # Client that requested this feature
+    client = models.ForeignKey(
+        # Note the departure for this signature
+        # This is the class for the relationship
+        'Client',
+        models.CASCADE,
+        # This is the human-readable name
+        verbose_name='Client'
+    )
+
+    # Numerical priority according to the client
+    client_priority = models.PositiveIntegerField('Client Priority')
+
+    # Expected completion date for request
+    target_date = models.DateField('Target Date')
+
+    # Store any URL associated with the request
+    ticket_url = models.URLField('Ticket URL', blank=True, default='')
+
+    # Which product area this request belongs to
+    # Defined as a foreign key to allow modification at runtime
+    product_area = models.ForeignKey(
+        'ProductArea',
+        on_delete=models.CASCADE,
+        verbose_name='Product Area'
+    )
+
+    def __str__(self):
+        return "'{title}' from {client}".format(title=self.title, client=self.client)
--- features/tests.py
+++ features/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from .models import Client, ProductArea
+from .models import Client, ProductArea, FeatureRequest
 
 class ModelSanityTests(TestCase):
     def test_client(self):
@@ -31,3 +31,37 @@ class ModelSanityTests(TestCase):
         assert loaded_area
         assert loaded_area.id == area.id
         assert loaded_area.name == area.name
+
+
+class FeatureRequestTests(TestCase):
+    def test_creation(self):
+        """Test basic creation API"""
+
+        title = 'Test feature title'
+        description = 'Test feature description.'
+
+        feature = FeatureRequest(
+            title=title,
+            description=description,
+            client=Client.objects.create(name='test_client'),
+            client_priority=1,
+            target_date='2016-1-1',
+            product_area=ProductArea.objects.create(name='test_area')
+        )
+
+        feature.save()
+
+        # Verify all data goes in and comes out the right way
+        assert feature.title == title
+
+        assert feature.description == description
+
+        self.assertIsInstance(feature.client, Client)
+        assert feature.client.name == 'test_client'
+
+        assert feature.client_priority == 1
+
+        assert feature.target_date == '2016-1-1'
+
+        self.assertIsInstance(feature.product_area, ProductArea)
+        assert feature.product_area.name == 'test_area'
