3816d9cda2ac1386381c3523e32a00e047b673b9
Kevin Ward <kevinward@fescorp.com>
2016-04-15 17:12:44 -0500

    Try restful.js for backend. Onto the testing!

---
+++ assets/js/api/backend.js
@@ -0,0 +1,25 @@
+/**
+Backend service
+
+This module defines endpoints and access functions the features app REST service
+*/
+
+/* window.fetch polyfill */
+import 'whatwg-fetch';
+import restful, {fetchBackend} from 'restful.js';
+
+
+/* Configure restful with fetch backend */
+const api = restful('/api', fetchBackend(fetch));
+
+const featureCollection = api.all('features');
+const featureMember = api.one('features');
+
+export default {
+    api,
+    endpoints: {
+        features: {
+            collection: featureCollection
+        }
+    }
+};
