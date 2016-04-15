0fda0eaa763f50d01880c580d6ff0b4c05046691
Kevin Ward <antiomiae@gmail.com>
2016-04-14 23:32:54 -0500

    Test vuex/store.js

---
+++ assets/tests/store_test.js
@@ -0,0 +1,34 @@
+import store from '../js/vuex/store.js';
+
+describe('store', function() {
+    it('should exist', () => {
+        expect(store).toBeDefined();
+    });
+
+    it('should have a state object', () => {
+        expect(store.state.features).toBeDefined();
+    });
+
+    it('should have a features array', () => {
+        expect(store.state.features).toBeDefined();
+    });
+
+    it('should have a dispatch method', () => {
+        // Is defined
+        expect(store.dispatch).toBeDefined();
+        // Is a function
+        expect(typeof store.dispatch).toEqual("function");
+    });
+
+    describe('mutations', function() {
+        describe('REPLACE', function() {
+            it('should replace store.state.features', () => {
+                const newGuy = [{}, {}, {}];
+
+                store.dispatch('REPLACE', newGuy);
+
+                expect(store.state.features).toEqual(newGuy);
+            });
+        });
+    });
+});
