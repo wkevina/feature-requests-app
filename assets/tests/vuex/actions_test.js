711ec066b1b5f2905e71d023511594a9fd4b4d41
Kevin Ward <antiomiae@gmail.com>
2016-04-19 22:55:09 -0500

    Fully test fetchFeatures action

--- assets/tests/vuex/actions_test.js
+++ assets/tests/vuex/actions_test.js
@@ -7,49 +7,84 @@ import 'jasmine-promises';
 const sinon = require('sinon');
 
 function mockBackend() {
+
     return {
-        endpoints: {
-            features: {
-                getAll: function() {}
-            },
-            client: {
-                getAll: function() {}
-            },
-            productArea: {
-                getAll: function() {}
+        //default: {
+            endpoints:  {
+                features: {
+                    getAll: function() {}
+                },
+                client: {
+                    getAll: function() {}
+                },
+                productArea: {
+                    getAll: function() {}
+                }
             }
-        }
+        //}
     };
 }
 
-function mockResponse(payload) {
+function mockResponse(payload, status=200) {
     return {
         body() {
             return {
                 data() {
-                    return {
-                        results: payload
-                    };
+                    return payload;
                 }
             };
+        },
+        statusCode() {
+            return status;
         }
     };
 }
 
-let actionsInjector = require('inject!../../js/vuex/actions.js');
+function delayed(setup, wrapup, delay=200) {
+    const promise = new Promise(function(resolve) {
+        setup();
+        setTimeout(resolve, delay);
+    }).then(wrapup);
 
-console.log(sinon);
+    return promise;
+}
+
+const actionsInjector = require('inject!../../js/vuex/actions.js');
 
 describe('fetchFeatures', function() {
     const backend = mockBackend(),
-          actions = actionsInjector({
-              '../api/backend.js': mockBackend()
-          });
+        actions = actionsInjector({
+            '../api/backend.js': backend
+        });
 
-    xit('should call features.getAll()', function() {
+    it('should call features.getAll()', function() {
         const getAll = sinon.stub().returns(Promise.resolve({}));
         backend.endpoints.features.getAll = getAll;
         actions.fetchFeatures({});
         expect(getAll.called).toBe(true);
+    });
+
+    it('should unwrap response and dispatch FEATURES_REPLACE', function() {
+        /* Mock response to return from endpoint */
+        const response = {results: {}},
+              /* Stub to emulate features endpoint */
+              getAll = sinon.stub().returns(
+                  Promise.resolve(mockResponse(response))
+              ),
+              /* Stub for store.dispatch */
+              dispatch = sinon.stub();
+
+        /* Stub the endpoint */
+        backend.endpoints.features.getAll = getAll;
+
+        return delayed(function() {
+            actions.fetchFeatures({dispatch});
+        }, function() {
+            /* Ensure correct args to dispatch */
+            expect(dispatch.calledWith(
+                'FEATURES_REPLACE',
+                response.results
+            )).toBe(true);
+        });
     });
 });
