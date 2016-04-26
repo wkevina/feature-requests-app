a3e13e38033d350061763d96098b6bf3f0f703b3
Kevin Ward <kevinward@fescorp.com>
2016-04-26 12:14:28 -0500

    Improve form layout

--- assets/js/components/featureform.vue
+++ assets/js/components/featureform.vue
@@ -2,13 +2,20 @@
   <div class="feature-form">
   <h4>Create new feature request</h4>
   <form>
+
+    <!-- Title -->
     <div class="row">
       <div class="col-md-12">
         <div class="form-group">
           <label for="feature-title">Title</label>
           <input type="text" id="feature-title" class="form-control">
         </div>
+      </div>
+    </div>
 
+    <!-- Description -->
+    <div class="row">
+      <div class="col-md-6">
         <div class="form-group">
           <label for="feature-description">Description</label>
           <textarea id="feature-description" class="form-control"></textarea>
@@ -16,8 +23,9 @@
       </div>
     </div>
 
+    <!-- Client -->
     <div class="row">
-      <div class="col-md-6">
+      <div class="col-md-3 col-sm-6">
         <div class="form-group">
           <label for="feature-client">Client</label>
           <select id="feature-client" class="form-control">
@@ -28,16 +36,16 @@
         </div>
       </div>
 
-      <div class="col-md-6">
+      <!-- Client Priority -->
+      <div class="col-md-3 col-sm-6">
         <div class="form-group">
-          <label for="feature-client-client">Priority</label>
+          <label for="feature-client-priority">Priority</label>
           <input id="feature-client-priority" type="number" class="form-control">
         </div>
       </div>
-    </div>
 
-    <div class="row">
-      <div class="col-md-6">
+      <!-- Product Area -->
+      <div class="col-md-3 col-sm-6">
         <div class="form-group">
           <label for="feature-product-area">Product Area</label>
           <select id="feature-product-area" class="form-control">
@@ -48,7 +56,8 @@
         </div>
       </div>
 
-      <div class="col-md-6">
+      <!-- Target Date -->
+      <div class="col-md-3 col-sm-6">
         <div class="form-group">
           <label for="feature-target-date">Target Date</label>
           <input id="feature-target-date" type="date" class="form-control">
