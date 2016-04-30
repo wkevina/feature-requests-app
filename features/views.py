25a65ef1978b28eee81c2163a8325d14565687c3
Kevin Ward <antiomiae@gmail.com>
2016-04-30 14:57:58 -0500

    Change create to handle conflicts

--- features/views.py
+++ features/views.py
@@ -2,25 +2,45 @@ from urllib.parse import urlparse
 
 from django.core.urlresolvers import resolve
 
+
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework.exceptions import ValidationError
+from rest_framework.settings import api_settings
 
 from .serializers import *
 from .models import FeatureRequest, Client, ProductArea
 from .service import shift_client_priority
 
+# TODO: get value programmatically from DRF
+NON_FIELD_ERRORS_KEY = api_settings.NON_FIELD_ERRORS_KEY
 
-def get_from_url(url):
-    """Return model associated with url"""
+
+def _resolve_url(url):
     o = urlparse(url)
     path = o.path
     # inspired by http://stackoverflow.com/questions/21406625/
     func, unused, kwargs = resolve(path)
 
+    return func, kwargs
+
+
+def get_from_url(url):
+    """Return model associated with url"""
+
+    func, kwargs = _resolve_url(url)
     model = func.cls.serializer_class.Meta.model
 
     return model.objects.get(pk=kwargs['pk'])
+
+
+def pk_from_url(url):
+    """Uses serializer to extract primary key"""
+
+    _, kwargs = _resolve_url(url)
+
+    return kwargs['pk']
 
 
 class FeatureRequestViewSet(viewsets.ModelViewSet):
@@ -80,6 +100,30 @@ class FeatureRequestViewSet(viewsets.ModelViewSet):
             else:
                 # have serializer raise exception
                 serializer.is_valid(raise_exception=True)
+
+
+    def create(self, request, *args, **kwargs):
+        try:
+            return super(FeatureRequestViewSet, self).create(request, args, kwargs)
+        except ValidationError as ex:
+            if NON_FIELD_ERRORS_KEY in ex.detail:
+                client_pk = pk_from_url(request.data['client'])
+                priority = int(request.data['client_priority'])
+
+                affected = shift_client_priority(
+                    priority,
+                    FeatureRequest.objects.filter(client__pk=client_pk))
+
+                response = super(FeatureRequestViewSet, self).create(
+                    request, args, kwargs)
+
+                # Inform client of models that have been modified
+                response['X-Also-Modified'] = [model.id for model in affected]
+
+                return response
+            else:
+                raise
+
 
 
 class ClientViewSet(viewsets.ModelViewSet):
