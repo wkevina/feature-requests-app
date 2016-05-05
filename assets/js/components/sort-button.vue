2ad77bdec14575d7c276db25b7cbb65268c43649
Kevin Ward <kevinward@fescorp.com>
2016-05-05 13:41:17 -0500

    Use font-awesome icons for sort-buttons

--- assets/js/components/sort-button.vue
+++ assets/js/components/sort-button.vue
@@ -1,16 +1,13 @@
 <template>
-  <button
-      @click="toggle"
-      class="btn"
-      :class="{'btn-primary': activated}"
-  >{{ title }}
+  <button @click="toggle" class="btn" :class="{'btn-primary': activated}">
+    {{ title }}&nbsp;
     <template v-if="activated">
-      &nbsp;
-      <span :class="{
-                   'glyphicon glyphicon-sort-by-alphabet': !sortReverse,
-                   'glyphicon glyphicon-sort-by-alphabet-alt': sortReverse,
-                   }">
-      </span>
+      <i :class="{ 'fa fa-sort-desc': !sortReverse,
+                   'fa fa-sort-asc': sortReverse }">
+      </i>
+    </template>
+    <template v-else>
+      <i class="fa fa-sort"></i>
     </template>
   </button>
 </template>
