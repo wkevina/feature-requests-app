86e183ea61922f4fac5884d38069dfe142ce3727
Kevin Ward <antiomiae@gmail.com>
2016-05-06 00:10:10 -0500

    Begin migration to postgres

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -89,14 +90,14 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+
+# Parse DATABASE_URL environment variable to configure database
+# postgres://USER:PASSWORD@HOST:PORT/NAME
+db_from_env = dj_database_url.config()
+
+DATABASES['default'] = db_from_env
 
 
 # Password validation
